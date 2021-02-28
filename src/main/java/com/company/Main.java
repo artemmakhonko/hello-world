ed6630ba217a06140b24f7109af227882f28a537
Artsem <artemmakhonko@gmail.com>
2021-02-28 14:34:40 -0600

    Added maven support

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(StringUtils.capitalize("hello world!"));
+    }
+}
